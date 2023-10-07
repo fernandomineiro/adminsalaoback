60a672ae57def7c500b51e92851cc897e2fb800d
Fernando Fernandes <fernando@parceirogoogle.com.br>
2023-10-07 19:45:19 -0300

    up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,13 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+    public function boot(UrlGenerator $url)
+    {
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
+    }
     /**
      * Register any application services.
      *
